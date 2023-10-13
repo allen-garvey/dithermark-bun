18b47559ef81e971b506cd880068acadfb28474d
Allen Garvey <allengarvey@hotmail.com>
2023-10-13 15:21:42 -0400

    Run beautify

--- src/webgl/filters.ts
+++ src/webgl/filters.ts
@@ -6,12 +6,28 @@ const FILTER_SHADERS_DIR = path.resolve(__dirname, '../../shaders/filters/');
 
 type RenderFunction = (gl: WebGLRenderingContext) => DrawFunction;
 
-const buildDrawFunc = (shaderPath: string, uniformNames: string[]): RenderFunction => (gl: WebGLRenderingContext): DrawFunction => createDrawImageFunc(gl, fs.readFileSync(path.resolve(FILTER_SHADERS_DIR, shaderPath)), uniformNames);
+const buildDrawFunc =
+    (shaderPath: string, uniformNames: string[]): RenderFunction =>
+    (gl: WebGLRenderingContext): DrawFunction =>
+        createDrawImageFunc(
+            gl,
+            fs.readFileSync(path.resolve(FILTER_SHADERS_DIR, shaderPath)),
+            uniformNames
+        );
 
-export const createSmoothDrawFunc = buildDrawFunc('smooth.glsl', ['u_radius', 'u_image_dimensions']);
+export const createSmoothDrawFunc = buildDrawFunc('smooth.glsl', [
+    'u_radius',
+    'u_image_dimensions',
+]);
 
-export const createBrightnessDrawFunc = buildDrawFunc('brightness.glsl', ['u_brightness']);;
+export const createBrightnessDrawFunc = buildDrawFunc('brightness.glsl', [
+    'u_brightness',
+]);
 
-export const createContrastDrawFunc = buildDrawFunc('contrast.glsl', ['u_contrast']);
+export const createContrastDrawFunc = buildDrawFunc('contrast.glsl', [
+    'u_contrast',
+]);
 
-export const createSaturationDrawFunc = buildDrawFunc('saturation.glsl', ['u_saturation']);
+export const createSaturationDrawFunc = buildDrawFunc('saturation.glsl', [
+    'u_saturation',
+]);
